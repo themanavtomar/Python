496df33d7c9ab38ccddd333ef440135901bdb608
Manav Tomar <142892116+themanavtomar@users.noreply.github.com>
2024-04-01 00:28:39 +0530

    Add files via upload

---
+++ #17 for loops.py
@@ -0,0 +1,20 @@
+name = "manav"
+for i in name:
+    print(i)
+# for i in name:
+#     print(i,end=" ")
+    if (i=="n"):
+        print("middle term:")
+print("this range of 5")
+for k in range(5):
+    print(k)
+print("this range of 10")
+for k in range(1,10):
+    print(k)
+print("table of 3")
+for k in range (0,33,3):
+    print(k)
+print("table of 15")
+for k in range (0,150,15):
+    print(k)
+print(15*10)
