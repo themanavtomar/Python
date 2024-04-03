c0e31a1f267656a073dc721db844ded4caf6f772
Manav Tomar <142892116+themanavtomar@users.noreply.github.com>
2024-04-04 01:43:45 +0530

    Add files via upload

---
+++ #24 Python Tuples.py
@@ -0,0 +1,8 @@
+#tuples are immutable
+tuple=(1,2,3,4,5)
+print(type(tuple))
+print(tuple)
+#tuple indexing same as list
+print(tuple[2])
+#range of index
+#syntax: Tuple[start:end:jumpindex] same as list
