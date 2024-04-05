6f41cd86cedcd910023248be5d67d5b439fb6e35
Manav Tomar <142892116+themanavtomar@users.noreply.github.com>
2024-04-05 18:10:57 +0530

    Add files via upload

---
+++ #29 docstring and pep8.py
@@ -0,0 +1,8 @@
+def square(n):
+    '''Takes in a number n, returns the square of n'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+"""# pep8 is the poem by python developers 
+# which is write in cmd 
+# by Step-1: python step-2:import this"""
